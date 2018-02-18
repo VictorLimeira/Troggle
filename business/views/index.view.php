d05ef4d0b60f4b9995e9ab7ed7ccf40e86c65777
VictorLimeira <mannueldp@gmail.com>
2018-02-18 20:47:36 -0300

    Show project index page

--- business/views/index.view.php
+++ business/views/index.view.php
@@ -1,5 +1,34 @@
 <?php require 'partials/head.php'; ?>
 
-        <h1>Troggle Index</h1>
+<main role="main" class="container">
+    <h1 class="mt-5 mb-4">TTroggle</h1>
+    <p class="lead">TTroggle is a learning purpose web system that mimics features from a service called Toggl.</p>
+
+    <blockquote class="blockquote text-center my-5">
+        <p class="mb-0">I am mesmerized about their product and decided to build a similar one during my process of learning.</p>
+        <footer class="blockquote-footer">Mannuel Victor</footer>
+    </blockquote>
+
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="card text-center my-4">
+                <div class="card-body">
+                    <h5 class="card-title">Login</h5>
+                    <p class="card-text">Login to start using TTroggle.</p><br>
+                    <a href="/login" class="btn btn-success">Login</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="card text-center  my-4">
+                <div class="card-body">
+                    <h5 class="card-title">Toggl</h5>
+                    <p class="card-text">Insanely simple time tracking, Toggl kills timesheets.</p>
+                    <a href="https://toggl.com/" class="btn btn-danger">Toggl</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
 
 <?php require 'partials/footer.php'; ?>
--- business/views/partials/head.php
+++ business/views/partials/head.php
@@ -8,6 +8,9 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
+        <!-- Custom styles for sticky footer -->
+        <link href="./business/views/resources/sticky-footer.css" rel="stylesheet">
+
         <title>TTroggle - track tasks</title>
     </head>
     <body>
